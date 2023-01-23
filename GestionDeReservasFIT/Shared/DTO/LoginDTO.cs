a184cbf814e44919e0448f41b97b01583ce0dbcb
Ivan David Carmona Herrera <56338454+ivandavidcarmonah@users.noreply.github.com>
2023-01-23 18:23:27 +0100

    login de usuario logear y deslogear acceso a la app, falta crear y token y autenticarlo, web de referencia de un ejemplo https://www.prowaretech.com/articles/current/blazor/wasm/jwt-authentication-simple

--- GestionDeReservasFIT/Shared/DTO/LoginDTO.cs
+++ GestionDeReservasFIT/Shared/DTO/LoginDTO.cs
@@ -4,25 +4,35 @@ namespace GestionDeReservasFIT.Shared.DTO
 {
     public class LoginDTO
     {
-        [Required]
-#pragma warning disable CS8618 // El elemento propiedad "UserName" que no acepta valores NULL debe contener un valor distinto de NULL al salir del constructor. Considere la posibilidad de declarar el elemento propiedad como que admite un valor NULL.
+        [Required(ErrorMessage = "UserName is required.")]
         public string UserName { get; set; }
-#pragma warning restore CS8618 // El elemento propiedad "UserName" que no acepta valores NULL debe contener un valor distinto de NULL al salir del constructor. Considere la posibilidad de declarar el elemento propiedad como que admite un valor NULL.
-        [Required]
-#pragma warning disable CS8618 // El elemento propiedad "Password" que no acepta valores NULL debe contener un valor distinto de NULL al salir del constructor. Considere la posibilidad de declarar el elemento propiedad como que admite un valor NULL.
+
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
-#pragma warning restore CS8618 // El elemento propiedad "Password" que no acepta valores NULL debe contener un valor distinto de NULL al salir del constructor. Considere la posibilidad de declarar el elemento propiedad como que admite un valor NULL.
         public bool RememberMe { get; set; }
+    }
+
+    public class LoginResult
+    {
+        public string message { get; set; }
+        public string email { get; set; }
+        public string jwtBearer { get; set; }
+        public bool success { get; set; }
+    }
+
+    public class RegModel : LoginDTO
+    {
+        [Required(ErrorMessage = "Confirm password is required.")]
+        [DataType(DataType.Password)]
+        [Compare("password", ErrorMessage = "Password and confirm password do not match.")]
+        public string confirmpwd { get; set; }
     }
 
     public class CurrentUser
     {
         public bool IsAuthenticated { get; set; }
-#pragma warning disable CS8618 // El elemento propiedad "UserName" que no acepta valores NULL debe contener un valor distinto de NULL al salir del constructor. Considere la posibilidad de declarar el elemento propiedad como que admite un valor NULL.
         public string UserName { get; set; }
-#pragma warning restore CS8618 // El elemento propiedad "UserName" que no acepta valores NULL debe contener un valor distinto de NULL al salir del constructor. Considere la posibilidad de declarar el elemento propiedad como que admite un valor NULL.
-#pragma warning disable CS8618 // El elemento propiedad "Claims" que no acepta valores NULL debe contener un valor distinto de NULL al salir del constructor. Considere la posibilidad de declarar el elemento propiedad como que admite un valor NULL.
         public Dictionary<string, string> Claims { get; set; }
-#pragma warning restore CS8618 // El elemento propiedad "Claims" que no acepta valores NULL debe contener un valor distinto de NULL al salir del constructor. Considere la posibilidad de declarar el elemento propiedad como que admite un valor NULL.
     }
 }
